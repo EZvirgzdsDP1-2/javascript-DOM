2fe5846ede8c90a376fb2b47759ac26fc3c45689
EZvirgzdsDP1-2 <ed.zvir04@inbox.lv>
2021-03-23 17:47:41 +0200

    Izpildiju 6.uzdevumu

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    var name_field = document.getElementById("vards");
+  
+    name_field.addEventListener("keyup", function() {
+  
+      var greeting_div = document.getElementById("sveiciens");
+  
+      greeting_div.innerHTML = ("Sveiki, " + name_field.value + "!");
+  
+    });
+  
+  });

5fa3c2823b4d58431229960b4f99d3ae3c619d1d
EZvirgzdsDP1-2 <ed.zvir04@inbox.lv>
2021-03-23 15:38:12 +0200

    Izpildiju 4.uzdevumu

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    document.getElementById("button").addEventListener("click",function(){
+
+    var hideClass=document.getElementsByClassName("toggle_me");
+    for(var i = 0; i < hideClass.length; i++)
+    {
+        if(hideClass[i].style.display=="none")
+        {
+            hideClass[i].style.display = "block";
+        }
+        else
+        {
+            hideClass[i].style.display="none";
+        }
+    }
+    });
+});

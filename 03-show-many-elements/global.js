a2c7b7138da2fc2261cd464649bdb454bdfbf0fd
EZvirgzdsDP1-2 <ed.zvir04@inbox.lv>
2021-03-30 16:52:25 +0200

    Palaboju 3. uzdevumu

--- 03-show-many-elements/global.js
+++ 03-show-many-elements/global.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(){
 
     document.getElementById("button").addEventListener("click", function(){
 
-        var hideClass=document.getElementsByClassName("show_me");
+        var hideClass = document.getElementsByClassName("show_me");
         
         for(var i = 0; i < hideClass.length; i++)
         {
